a34f33a7cf7e3ebb768ed0a3e5e4223f6600466a
Safwan Islam <saislam@davidson.edu>
2022-06-02 18:44:37 -0500

    continued working on local storage

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-var cityEl = document.querySelector('#city-form');
+var cityFormEl = document.querySelector('#city-form');
+var cityNameEl = document.querySelector('#city');
+var weatherEl = document.querySelector('#weather');
+var weatherContainerEl = document.querySelector('#weather-container');
+var forecaseContainerEl = document.querySelector('#forecast-container');
+var citySearchEl = document.querySelector('#city-searches');
 
+var formSubmitHandler = function (event) {
+    event.preventDefault();
 
-function init() {
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=Wichita&limit=1&appid=afbc3e766ad7b125ff6728193711f7c7')
+    var cityName = cityNameEl.value.trim();
+
+    if (cityName) {
+        getWeather(cityName);
+
+        weatherContainerEl.textContent = '';
+        cityNameEl.value = '';
+    } else {
+        alert('Please enter a valid city name!')
+    }
+}
+
+var getWeatherOfCity = function (city) {
+    var apiCity = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=afbc3e766ad7b125ff6728193711f7c7'
+    fetch(apiCity)
         .then(function (response) {
-            return response.json();
+            console.log(response);
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(response.statusText)
+            }
         })
         .then(function (data) {
+            storeCity(city);
             getWeather(data[0].lat, data[0].lon);
+        })
+        .catch(function(error) {
+            alert(error);
         })
 }
 
@@ -38,5 +67,29 @@ function getWeather(lat, lon) {
                 console.log(day);
             }
         })
+    }
+var storeCity = function (city) {
+    var cities = JSON.parse(localStorage.get('cities')) || [];
+    if (!cities.includes(city)) {
+        cities.push(city);
+        localStorage.setItem('cities', JSON.stringify(cities));
+        displayPreviousCitiesButtons();
+    }
 }
-init();
+
+var displayPreviousCitiesButtons = function () {
+    var cities = JSON.parse(localStorage.get('cities')) || [];
+    citySearchEl.innerHTML = '';
+    for (var city of cities) {
+        var cityBtn = document.createElement('button');
+        cityBtn.dataset.city = city;
+        cityBtn.className = 'btn';
+        cityBtn.textContent = city;
+        citySearchEl.append(cityBtn);
+    }
+}
+
+
+
+cityFormEl.addEventListener('submit', formSubmitHandler);
+displayPreviousCitiesButtons;
